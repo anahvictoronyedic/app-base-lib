419e505026c39c0e3c0f671bc9d1d026135c2592
Anah Victor <anahvictoronyedic@gmail.com>
2022-03-27 22:52:07 +0000

    made some changes

--- src/abstracts/types/common.ts
+++ src/abstracts/types/common.ts
@@ -4,3 +4,9 @@ export type ID = number|string;
 export type PLAIN_OBJECT<D=any> = {[index:string]:D}
 
 export enum CRUD { CREATE = 1 , READ , UPDATE , DELETE };
+
+export type MODAL_RESULT<D,ERR> = PLAIN_OBJECT & {
+    data:D,
+    err?:ERR,
+    reason:'close'|'success'|'failure',
+};
